0d3fcd2ab15de201a3e504592c15c35c6299b686
prashobh4657 <prashobh4657@gmail.com>
2026-02-22 09:38:24 +0530

    Feature flag Demo updated

--- src/featureFlag/FeatureFlagConstants.java
+++ src/featureFlag/FeatureFlagConstants.java
@@ -9,9 +9,9 @@ public final class FeatureFlagConstants {
     public static final String FEATURE_NEW_CHECKOUT = "new-checkout";
     public static final String FEATURE_LOCATION_BASED_OFFER = "location-based-offer";
 
-    public static final String USER_ID_1 = "u1";
-    public static final String USER_ID_2 = "u2";
-    public static final String USER_ID_3 = "u3";
+    public static final String USER_ID_1 = "User1";
+    public static final String USER_ID_2 = "User2";
+    public static final String USER_ID_3 = "User3";
 
     public static final String CITY_BENGALURU = "Bengaluru";
     public static final String CITY_MUMBAI = "Mumbai";
